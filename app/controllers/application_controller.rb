a79dd03f3e0780d3a44af31078ef06c82dd414b1
suresh <suresh_torlapati@yahoo.com>
2012-09-14 14:44:57 +0530

    validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   protect_from_forgery
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, :alert => exception.message
+  end
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,6 @@
 class Ability
   include CanCan::Ability
+  
   def initialize(user)
        if user.admin?
         can :manage, :all
